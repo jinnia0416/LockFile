75081a3ca2c0d10a00b17ff78684e561ac75c790
BOBO <>
2021-06-14 10:42:07 +0800

    首次簽入 鎖定檔案

---
+++ LockFile/Main.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LockFile
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txbSelectPath = new System.Windows.Forms.TextBox();
+            this.labSelectFileCount = new System.Windows.Forms.Label();
+            this.btnLockFile = new System.Windows.Forms.Button();
+            this.btnUnLockFile = new System.Windows.Forms.Button();
+            this.labLockIng = new System.Windows.Forms.Label();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(417, 27);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "選擇檔案";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txbSelectPath
+            // 
+            this.txbSelectPath.Location = new System.Drawing.Point(12, 12);
+            this.txbSelectPath.Multiline = true;
+            this.txbSelectPath.Name = "txbSelectPath";
+            this.txbSelectPath.ReadOnly = true;
+            this.txbSelectPath.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbSelectPath.Size = new System.Drawing.Size(399, 54);
+            this.txbSelectPath.TabIndex = 1;
+            // 
+            // labSelectFileCount
+            // 
+            this.labSelectFileCount.AutoSize = true;
+            this.labSelectFileCount.Location = new System.Drawing.Point(10, 89);
+            this.labSelectFileCount.Name = "labSelectFileCount";
+            this.labSelectFileCount.Size = new System.Drawing.Size(77, 12);
+            this.labSelectFileCount.TabIndex = 2;
+            this.labSelectFileCount.Text = "合計選取: 0筆";
+            // 
+            // btnLockFile
+            // 
+            this.btnLockFile.Location = new System.Drawing.Point(330, 78);
+            this.btnLockFile.Name = "btnLockFile";
+            this.btnLockFile.Size = new System.Drawing.Size(81, 34);
+            this.btnLockFile.TabIndex = 3;
+            this.btnLockFile.Text = "鎖定檔案";
+            this.btnLockFile.UseVisualStyleBackColor = true;
+            this.btnLockFile.Click += new System.EventHandler(this.btnLockFile_Click);
+            // 
+            // btnUnLockFile
+            // 
+            this.btnUnLockFile.Location = new System.Drawing.Point(417, 78);
+            this.btnUnLockFile.Name = "btnUnLockFile";
+            this.btnUnLockFile.Size = new System.Drawing.Size(81, 34);
+            this.btnUnLockFile.TabIndex = 4;
+            this.btnUnLockFile.Text = "解鎖檔案";
+            this.btnUnLockFile.UseVisualStyleBackColor = true;
+            this.btnUnLockFile.Click += new System.EventHandler(this.btnUnLockFile_Click);
+            // 
+            // labLockIng
+            // 
+            this.labLockIng.AutoSize = true;
+            this.labLockIng.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labLockIng.ForeColor = System.Drawing.Color.Red;
+            this.labLockIng.Location = new System.Drawing.Point(143, 74);
+            this.labLockIng.Name = "labLockIng";
+            this.labLockIng.Size = new System.Drawing.Size(135, 29);
+            this.labLockIng.TabIndex = 5;
+            this.labLockIng.Text = "鎖定中!!";
+            this.labLockIng.Visible = false;
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(LockFile.Program);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 120);
+            this.Controls.Add(this.labLockIng);
+            this.Controls.Add(this.btnUnLockFile);
+            this.Controls.Add(this.btnLockFile);
+            this.Controls.Add(this.labSelectFileCount);
+            this.Controls.Add(this.txbSelectPath);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txbSelectPath;
+        private System.Windows.Forms.Label labSelectFileCount;
+        private System.Windows.Forms.Button btnLockFile;
+        private System.Windows.Forms.Button btnUnLockFile;
+        private System.Windows.Forms.Label labLockIng;
+        private System.Windows.Forms.BindingSource programBindingSource;
+    }
+}
+
---
+++ LockFile/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LockFile
+{
+    public partial class Main : Form
+    {
+        //選取檔案陣列
+        private string[] filePaths = new string[0];
+        //鎖定檔案讀取流陣列
+        private FileStream[] fileStream;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "鎖定檔案";
+            btnUnLockFile.Enabled = false;
+        }
+
+        //選取檔案
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//該值確定是否可以選擇多個檔案
+            dialog.Title = "請選擇資料夾";
+            dialog.Filter = "所有檔案(*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //儲存選取的檔案
+                filePaths = dialog.FileNames;
+                //將選取的檔案顯示到畫面上
+                txbSelectPath.Text = string.Join(Environment.NewLine, filePaths);
+                labSelectFileCount.Text = $"合計選取: {filePaths.Length}筆";
+            }
+        }
+        //鎖定檔案
+        private void btnLockFile_Click(object sender, EventArgs e)
+        {
+            if(filePaths.Length < 1)
+            {
+                MessageBox.Show("請選擇檔案後在按鎖定");
+                return;
+            }
+            //將其他按鈕鎖定
+            btnLockFile.Enabled = false;
+            btnSelectFile.Enabled = false;
+
+            btnUnLockFile.Enabled = true;
+            labLockIng.Visible = true;
+
+            //初始化陣列
+            fileStream = new FileStream[filePaths.Length];
+            for(int i= 0; i< filePaths.Length; i++)
+            {
+                fileStream[i] = new FileStream(filePaths[i], FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+        }
+
+        private void btnUnLockFile_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < fileStream.Length; i++)
+            {
+                fileStream[i].Close();
+            }
+            fileStream = null;
+
+            btnLockFile.Enabled = true;
+            btnSelectFile.Enabled = true;
+
+            btnUnLockFile.Enabled = false;
+            labLockIng.Visible = false;
+        }
+    }
+}
